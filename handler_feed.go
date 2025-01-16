a15481c6af0eac144337ccde43ae3a80b29af7eb
Prashant singh <135175230+prashant231203@users.noreply.github.com>
2025-01-16 20:08:32 +0530

    new

---
+++ handler_feed.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+
+	"github.com/prashant231203/go-Aggregator-project/internal/database"
+)
+
+func (apicfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request,user database.User) {
+	type parameters struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	}
+
+	feed, err := apicfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+		Url:       params.URL,
+		UserID:     user.ID
+	})
+	if err != nil {
+		respondWithError(w, 500, fmt.Sprintf("Error creating user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apicfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
+	respondWithJSON(w, 200, databaseUserToUser(user))
+}
