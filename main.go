2fd402c8b077fddb283b9687616da71464e6115a
Prashant singh <135175230+prashant231203@users.noreply.github.com>
2025-01-16 12:40:17 +0530

    new

--- main.go
+++ main.go
@@ -61,6 +61,7 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 	v1Router.Post("/users", apicfg.handlerCreateUser)
+	v1Router.Get("/users", apicfg.handlerGetUser)
 
 	router.Mount("/v1", v1Router)
 
