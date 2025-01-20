f38a3292d1fb0613769f113930da59a83c95e9da
Prashant singh <135175230+prashant231203@users.noreply.github.com>
2025-01-20 07:56:09 +0530

    new

--- sql/schema/006_posts.sql
+++ sql/schema/006_posts.sql
@@ -1,13 +1,13 @@
 -- +goose Up
 CREATE TABLE posts(
     id UUID PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    created_at TIMESTAMP NOT NULL, 
+    updated_at TIMESTAMP NOT NULL,
     title TEXT NOT NULL,
     description TEXT,
     published_at TIMESTAMP NOT NULL,
-    url TEXT NOT NULL UNIQUE, -- Added `TEXT` data type
-    feed_id UUID NOT NULL REFERENCES feed(id) ON DELETE CASCADE
+    url TEXT NOT NULL UNIQUE,
+    feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE
 );
 
 -- +goose Down
