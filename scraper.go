68a1e414f9afc450040319d453fdc85bf742e3d0
Prashant singh <135175230+prashant231203@users.noreply.github.com>
2025-01-20 08:48:44 +0530

    new

--- scraper.go
+++ scraper.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
@@ -72,6 +73,9 @@ func scrapeFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 				FeedID:      feed.ID,
 			})
 		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key") {
+				continue
+			}
 			log.Println("failed to create a post:", err)
 		}
 	}
